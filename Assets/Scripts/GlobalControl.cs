0972b052072f5e6cee2dbc052b4db6838be889a4
wjl <411046642@qq.com>
2021-03-25 23:28:30 +0800

    修改单例模式的写法，将_instance变为私有

--- Assets/Scripts/GlobalControl.cs
+++ Assets/Scripts/GlobalControl.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GlobalControl : MonoBehaviour
 {
     //单例模式,控制全局
-    public static GlobalControl Instance;//控制的唯一单例
+    private static GlobalControl _instance;//控制的唯一单例
+    public static GlobalControl Instance { get { return _instance; } }
     public int BattleSceneindex;//怪物类型场景
     public int PlayerPosindex;//玩家地图位置
     public int TeamLeftHP;//联合部队剩余HP
@@ -13,18 +14,18 @@ public class GlobalControl : MonoBehaviour
 
     private void Awake()
     {
-        if(Instance == null)
+        if (_instance != null && _instance != this)
         {
+            Destroy(this.gameObject);
+        } 
+        else 
+        {
+            _instance = this;
             DontDestroyOnLoad(gameObject);
-            Instance = this;
             Instance.BattleSceneindex = 0;
             Instance.playerpos = new Vector3(-2.12f,-1.02f,0);
             Instance.TeamLeftHP = 100000;
             Instance.PlayerPosindex = 0;
-        }
-        else if(Instance != null)
-        {
-            Destroy(gameObject);
         }
     }
 }
